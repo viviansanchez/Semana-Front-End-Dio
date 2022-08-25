c5953cba9efc1335f00712653ffcbcf55571fc2a
Vivian Sanchez Desktop <visasal88@gmail.com>
2022-08-25 19:33:13 -0300

    adicionando firebase

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,35 @@
+
+
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyCHdFNj5wdz6BL_MIfSYfxrmvYSYyVWMJY",
+
+    authDomain: "semana-front-end-dio.firebaseapp.com",
+
+    projectId: "semana-front-end-dio",
+
+    storageBucket: "semana-front-end-dio.appspot.com",
+
+    messagingSenderId: "201643930209",
+
+    appId: "1:201643930209:web:a46b52639bcbe32f8ff020"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+import { subscribeToHellfireClub, getHellfireClubSubscriptions } from './firebase/hellfire-club.js'
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', async () => {
+    const subscription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    const subscriptionId = await subscribeToHellfireClub(subscription)
+    // console.log(`Inscrito com sucesso: ${subscriptionId}`)
+
+    txtName.value = ''
+    txtEmail.value = ''
+    txtLevel.value = ''
+    txtCharacter.value = ''
+
+    // alert(`Inscrito com sucesso: ${subscriptionId}`)
+}) 
+
+async function loadData() {
+
+    const subscriptions =  await getHellfireClubSubscriptions()
+    console.log(subscriptions)
+}
+
+loadData()
